1d40068626fc20b9a75f8d245f4d479a7a102bf2
overmind1 <sharp.maestro@gmail.com>
2012-07-31 15:53:26 +0400

    unst

--- src/main/java/tap/execounting/dal/mediators/ContractMediator.java
+++ src/main/java/tap/execounting/dal/mediators/ContractMediator.java
@@ -1,11 +1,15 @@
 package tap.execounting.dal.mediators;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 import tap.execounting.dal.CrudServiceDAO;
+import tap.execounting.dal.QueryParameters;
 import tap.execounting.data.ContractState;
 import tap.execounting.entities.Client;
 import tap.execounting.entities.Contract;
@@ -15,94 +19,176 @@ import tap.execounting.entities.EventType;
 import tap.execounting.entities.Payment;
 import tap.execounting.entities.Teacher;
 
-public class ContractMediator implements ContractMed{
-	
+public class ContractMediator implements ContractMed {
+
 	@Inject
 	private CrudServiceDAO dao;
 
+	private Contract unit;
+
 	public Contract getUnit() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return unit;
+		} catch (NullPointerException npe) {
+			return null;
+		}
 	}
 
 	public void setUnit(Contract unit) {
-		// TODO Auto-generated method stub
-		
+		this.unit = unit;
 	}
 
 	public String getTeacherName() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			String name;
+			// way1
+			name = unit.getTeacher().getName();
+			// way2
+			// name = dao.find(Teacher.class, unit.getTeacherId()).getName();
+			return name;
+		} catch (NullPointerException npe) {
+			npe.printStackTrace();
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public EventType getEventType() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			EventType et;
+			// way1
+			et = unit.getEventType();
+			// way2
+			// et = dao.find(EventType.class, unit.getTypeId());
+			return et;
+		} catch (NullPointerException npe) {
+			npe.printStackTrace();
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public ContractType getContractType() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			ContractType ct;
+			// way1
+			ct = unit.getContractType();
+			// way2
+			// ct = dao.find(ContractType.class, unit.getContractTypeId());
+			return ct;
+		} catch (NullPointerException npe) {
+			npe.printStackTrace();
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public Date getDate() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return unit.getDate();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public int getLessonsNumber() {
-		// TODO Auto-generated method stub
-		return 0;
+		try {
+			return unit.getLessonsNumber();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			return 0;
+		}
 	}
 
 	public int getPrice() {
-		// TODO Auto-generated method stub
-		return 0;
+		try {
+			return unit.getMoney();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			return 0;
+		}
+
 	}
 
 	public int getBalance() {
-		// TODO Auto-generated method stub
-		return 0;
+		try {
+			return unit.getBalance();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			return 0;
+		}
 	}
 
 	public List<Event> getEvents() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			List<Event> events;
+			events = unit.getEvents();
+			return events;
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public List<Payment> getPayments() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return unit.getPayments();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
+	
+	private List<Contract> cache;
+	private Map<String, Object> appliedFilters;
 
 	public List<Contract> getGroup() {
-		// TODO Auto-generated method stub
-		return null;
+		if(cache==null)		
+			load();
+		return cache;
+	}
+	
+	private void load(){
+		cache = dao.findWithNamedQuery(Contract.ALL);
+		appliedFilters = new HashMap<String, Object>();
 	}
 
 	public void setGroup(List<Contract> group) {
-		// TODO Auto-generated method stub
-		
+cache = group;
 	}
 
 	public List<Contract> getAllContracts() {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.findWithNamedQuery(Contract.ALL);
 	}
 
 	public void reset() {
-		// TODO Auto-generated method stub
-		
+		load();
 	}
 
 	public String getFilterState() {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sb = new StringBuilder();
+		for(Entry<String, Object> entry : appliedFilters.entrySet())
+			sb.append(entry.getKey() + ": " + entry.getValue().toString() + "\n");
+		return sb.toString();
 	}
 
 	public ContractMed filter(Client c) {
-		// TODO Auto-generated method stub
-		return null;
+		appliedFilters.put("Client", c);
+		Contract con;
+		for(int i = cache.size()-1;i>=0;i--){
+			con = cache.get(i);
+			if(con.getClientId()==c.getId())
+				continue;
+			cache.remove(i);
+			}
+		return this;
 	}
 
 	public ContractMed filter(Teacher t) {
--- src/main/java/tap/execounting/dal/mediators/TeacherMedImpl.java
+++ src/main/java/tap/execounting/dal/mediators/TeacherMedImpl.java
@@ -16,130 +16,110 @@ import tap.execounting.entities.Teacher;
 
 public class TeacherMedImpl implements TeacherMed {
 
+	@Inject
+	private CrudServiceDAO dao;
+
+	public Teacher unit;
+
+	public List<Teacher> getAllTeachers() {
+		return dao.findWithNamedQuery(Teacher.ALL);
+	}
+
 	public Teacher getUnit() {
-		// TODO Auto-generated method stub
-		return null;
+		return unit;
+	}
+
+	public List<Contract> getAllContracts() {
+		return dao.findWithNamedQuery(Contract.WITH_TEACHER, QueryParameters
+				.with("teacherId", unit.getId()).parameters());
+	}
+
+	public List<Contract> getActualContracts() {
+		List<Contract> list = getAllContracts();
+		for (int i = list.size() - 1; i >= 0; i--)
+			if (!list.get(i).isActive())
+				list.remove(i);
+		return list;
+	}
+
+	public List<Client> getAllClients() {
+		Set<Client> clients = new HashSet<Client>(10);
+		for (Contract c : getAllContracts())
+			clients.add(c.getClient());
+		return new ArrayList<Client>(clients);
+	}
+
+	public List<Client> getActiveClients() {
+		Set<Client> clients = new HashSet<Client>(10);
+		for (Contract c : getActualContracts())
+			clients.add(c.getClient());
+		return new ArrayList<Client>(clients);
 	}
 
 	public void setUnit(Teacher unit) {
-		// TODO Auto-generated method stub
-		
+		this.unit = unit;
+	}
+
+	public String worksOn(String day) {
+		Integer code = null;
+		if (day.equals("Пн"))
+			code = unit.getMonday();
+		else if (day.equals("Вт"))
+			code = unit.getTuesday();
+		else if (day.equals("Ср"))
+			code = unit.getWednesday();
+		else if (day.equals("Чт"))
+			code = unit.getThursday();
+		else if (day.equals("Пт"))
+			code = unit.getFriday();
+		else if (day.equals("Сб"))
+			code = unit.getSaturday();
+		else if (day.equals("Вс"))
+			code = unit.getSunday();
+
+		return code == null ? "-" : dao.find(Facility.class, code).getName();
 	}
 
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return unit.getName();
+		} catch (NullPointerException e) {
+			return null;
+		}
 	}
 
 	public Integer[] getSchedule() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Integer[] sched = new Integer[7];
+			sched[0] = unit.getMonday();
+			sched[1] = unit.getTuesday();
+			sched[2] = unit.getWednesday();
+			sched[3] = unit.getThursday();
+			sched[4] = unit.getFriday();
+			sched[5] = unit.getSaturday();
+			sched[6] = unit.getSunday();
+			return sched;
+		} catch (NullPointerException e) {
+			return null;
+		}
 	}
 
 	public boolean getState() {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			return unit.isActive();
+		} catch (NullPointerException npe) {
+			return false;
+		}
 	}
 
-	public List<Teacher> getAllTeachers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	private List<Teacher> teachersCache;
 
 	public Integer countGroupSize() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return teachersCache.size();
+		} catch (NullPointerException npe) {
+			return null;
+		}
 	}
-//	
-//	@Inject
-//	private CrudServiceDAO dao;
-//	
-//	public Teacher unit;
-//	
-//	
-//	public List<Teacher> getAllTeachers(){
-//		return dao.findWithNamedQuery(Teacher.ALL);
-//	}
-//	
-//	public Teacher getUnit(){
-//		return unit;
-//	}
-//	
-//	public List<Contract> getAllContracts(){
-//		return dao.findWithNamedQuery(Contract.WITH_TEACHER,
-//				QueryParameters.with("teacherId", unit.getId()).parameters());
-//	}
-//	
-//	public List<Contract> getActualContracts(){
-//		List<Contract> list= getAllContracts();
-//		for(int i=list.size()-1;i>=0;i--)
-//			if(!list.get(i).isActive())
-//				list.remove(i);
-//		return list;
-//	}
-//	
-//	public List<Client> getAllClients(){
-//		Set<Client> clients = new HashSet<Client>(10);
-//		for(Contract c : getAllContracts())
-//			clients.add(c.getClient());
-//		return new ArrayList<Client>(clients);
-//	}
-//	
-//	public List<Client> getActiveClients(){
-//		Set<Client> clients = new HashSet<Client>(10);
-//		for(Contract c : getActualContracts())
-//			clients.add(c.getClient());
-//		return new ArrayList<Client>(clients);
-//	}
-//	
-//	public void setUnit(Teacher unit){
-//		this.unit = unit;
-//	}
-//	
-//	public String worksOn(String day){
-//		Integer code = null;
-//		if(day.equals("Пн"))
-//			code=unit.getMonday();
-//		else
-//		if(day.equals("Вт"))
-//			code=unit.getTuesday();
-//		else
-//		if(day.equals("Ср"))
-//			code=unit.getWednesday();
-//		else
-//		if(day.equals("Чт"))
-//			code=unit.getThursday();
-//		else
-//		if(day.equals("Пт"))
-//			code=unit.getFriday();
-//		else
-//		if(day.equals("Сб"))
-//			code=unit.getSaturday();
-//		else
-//		if(day.equals("Вс"))
-//			code=unit.getSunday();
-//		
-//		return code==null ? "-" : dao.find(Facility.class, code).getName();
-//	}
-//
-//	public String getName() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	public Integer[] getSchedule() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	public boolean getState() {
-//		// TODO Auto-generated method stub
-//		return false;
-//	}
-//
-//	public Integer countGroupSize() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
 
 }
